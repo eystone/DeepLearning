a1cd9c1fef7773a7ce177d9451070a59166f3f5a
eystone <thomas.noe@etu.univ-amu.fr>
2020-10-21 12:01:55 +0200

    Patch readme

--- main.cpp
+++ main.cpp
@@ -26,20 +26,20 @@ bool InitCapture (struct SimpleCapParams & capture)
 int main()
 {
     struct SimpleCapParams capture;
-    capture.mWidth = 24;
-    capture.mHeight = 18;
-    capture.mTargetBuf = new int[24 * 18];
+    capture.mWidth = 50;
+    capture.mHeight = 20;
+    capture.mTargetBuf = new int[50 * 20];
 
     if ( !InitCapture(capture) ) { return -1; }
 
     //Test a Ascii art
     char light[] = ".,-o+O0@";
-    for (int i(0); i < 18; i++)
+    for (int i(0); i < 20; i++)
     {
         //">> 13" Decale de 13 Bits vers la droite afin de trouver les bits de vert
         //"& 7" met un filtre &0111 afin de garder que les bits que l'on a decalé
         // utilise ces bits en tant qu'index dans notre chaine de car light
-        for (int j(0); j < 24; j++) printf("%c", light[(capture.mTargetBuf[i*24+j] >> 13) & 7]);
+        for (int j(0); j < 50; j++) printf("%c", light[(capture.mTargetBuf[i*50+j] >> 13) & 7]);
         std::cout << std::endl;
     }
     deinitCapture(0);

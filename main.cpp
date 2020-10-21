c3b74c9cf7f9c1eda94943329284586cdb368896
eystone <thomas.noe@etu.univ-amu.fr>
2020-10-21 11:18:55 +0200

    Initial commit : Ascii art

---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+#include "escapi.h"
+
+bool InitCapture (struct SimpleCapParams & capture)
+{
+    int devices = setupESCAPI(); // init lib
+    if (devices == 0)
+    {
+        std::cerr << "No webcam found" << std::endl;
+        return false;
+    }
+    if (initCapture(0, &capture) == 0)
+    {
+        std::cerr << "Can't init Capture" << std::endl;
+        return false;
+    }
+    for (int i(0); i < 5; i++) // do 5 loops for do light calib
+    {
+        doCapture(0);
+        while (!isCaptureDone(0)){} //Doing Capture         /!\ if webcam disconnect during this, INFINITE LOOP
+    }
+    return true;
+}
+
+int main()
+{
+    struct SimpleCapParams capture;
+    capture.mWidth = 24;
+    capture.mHeight = 18;
+    capture.mTargetBuf = new int[24 * 18];
+
+    if ( !InitCapture(capture) ) { return -1; }
+
+    //Test a Ascii art
+    char light[] = ".,-o+O0@";
+    for (int i(0); i < 18; i++)
+    {
+        //">> 13" Decale de 13 Bits vers la droite afin de trouver les bits de vert
+        //"& 7" met un filtre &0111 afin de garder que les bits que l'on a decalé
+        // utilise ces bits en tant qu'index dans notre chaine de car light
+        for (int j(0); j < 24; j++) printf("%c", light[(capture.mTargetBuf[i*24+j] >> 13) & 7]);
+        std::cout << std::endl;
+    }
+    deinitCapture(0);
+    return 0;
+}

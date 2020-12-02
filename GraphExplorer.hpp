3904e2108f12d26edcf91afd16fa5ddabb8446b2
Thomas Noé <46630785+eystone@users.noreply.github.com>
2020-12-02 17:58:35 +0100

    Finitions detection code bar

--- GraphExplorer.hpp
+++ GraphExplorer.hpp
@@ -1,62 +1,51 @@
 #pragma once
 #include "DataStructures.hpp"
 
-void ParcourirGraph(std::map<Node*, bool>& visited, Node* node, std::string& Barcode) {
+unsigned ParcourirGraph(std::map<Node*, bool>& visited, Node* node, unsigned valeur) {
     visited[node] = true;
     for (std::pair<Node*, int> pairNode : node->getNeighbors()) {
         if (!visited[pairNode.first]) {
+            
+            if (pairNode.second > 150 || pairNode.second < -150) valeur++;
 
-            Barcode += (pairNode.first->getMono() < 130) ? '1' : '0';
-
-            ParcourirGraph(visited, pairNode.first, Barcode);
+            return ParcourirGraph(visited, pairNode.first, valeur);
         }
     }
+    return valeur;
 }
 
-std::string ProcessBarcode(const std::string& Barcode) {
-    std::string processedBarcode("");
-    bool countB(false);
-    unsigned count(0);
-    for (unsigned i(0); i < Barcode.size(); i++) {
-        if (Barcode[i] == '1') {
-            countB = true;
-            count += 1;
-        }
-        else if (countB) {
-            processedBarcode += (count > 1) ? '1' : '0';
-            countB = false;
-            count = 0;
-        }
-    }
-    return processedBarcode;
-}
-
-std::string ReadBarcode(Graph& graph, const unsigned& mW, const unsigned& mH) {
+bool HaveBarcode(Graph& graph, const unsigned& mW, const unsigned& mH) {
     Graph BarcodeGraph;
 
-    //Create graph with draw Line
-    for (unsigned i = 0; i < 4; i = i + 1) {
-        for (unsigned x = 0; x < mW; x = x + 1) {
-            BarcodeGraph.addNode(x + i * mW, graph.getNodes()[x + ((mH / 3) + i * 4) * mW]->getMono());
-        }
-    }
-
-    for (Node* node : BarcodeGraph.getNodes()) {
-        unsigned x = (node->getUId() % mW);
-        unsigned y = (node->getUId() / mW);
-
-        if ((x + 1) + y * mW < BarcodeGraph.getNodes()[BarcodeGraph.getNodes().size()-1]->getUId())
-            BarcodeGraph.addEdge(x + y * mW, (x + 1) + y * mW);
-    }
-    //BarcodeGraph.printGraph();
-    //BarcodeGraph.drawImage(mW);
-
-    //Process graph to find result
-    std::string Barcode;
     std::map<Node*, bool> visitedNodes;
-    ParcourirGraph(visitedNodes, BarcodeGraph.getNodes()[0], Barcode);
-
-    std::cout << Barcode << std::endl;
-
-    return ProcessBarcode(Barcode);
+    unsigned change = ParcourirGraph(visitedNodes, graph.getNodes()[0], 0);
+    std::cout << "change: " << change << "size: " << graph.getNodes().size() << std::endl;
+    
+    //on regarde si le nombre de changement brutaux est équivalent a 15% des nodes du graph alors on considere que c'est un code barre
+    return (change > graph.getNodes().size()*0.15) ? true : false;
 }
+
+
+
+
+
+/*
+//Create graph with draw Line
+for (unsigned i = 0; i < 4; i = i + 1) {
+    for (unsigned x = 0; x < mW; x = x + 1) {
+        BarcodeGraph.addNode(x + i * mW, graph.getNodes()[x + ((mH / 3) + i * 4) * mW]->getMono());
+    }
+}
+
+for (Node* node : BarcodeGraph.getNodes()) {
+    unsigned x = (node->getUId() % mW);
+    unsigned y = (node->getUId() / mW);
+
+    if ((x + 1) + y * mW < BarcodeGraph.getNodes()[BarcodeGraph.getNodes().size()-1]->getUId())
+        BarcodeGraph.addEdge(x + y * mW, (x + 1) + y * mW);
+}
+BarcodeGraph.printGraph();
+BarcodeGraph.drawImage(mW);
+*/
+
+//Process graph to find result
--- main.cpp
+++ main.cpp
@@ -22,14 +22,17 @@ bool InitCapture(struct SimpleCapParams& capture) {
     return true;
 }
 
+//Create a classic graph
 void InitGraph(Graph& graph, const struct SimpleCapParams& capture) {
 
+    //Add nodes
     for (int i(0); i < capture.mHeight; i++) {
         for (int j(0); j < capture.mWidth; j++) {
-            graph.addNode(unsigned(i * capture.mWidth + j), ConvertRGBBinToMono(capture.mTargetBuf[i * capture.mWidth + j]));
+            graph.addNode(unsigned(i * capture.mWidth + j), ConvertRGBBinToMono(capture.mTargetBuf[i * capture.mWidth + j]) );
         }
     }
 
+    //Add edges all connected
     for (unsigned i = 0; i < graph.getNodes().size(); i++) {
         unsigned x = (i % unsigned(capture.mWidth));
         unsigned y = (i / unsigned(capture.mWidth));
@@ -47,11 +50,46 @@ void InitGraph(Graph& graph, const struct SimpleCapParams& capture) {
     }
 }
 
+std::vector<Graph> SubdivideGraph(Graph& graph) {
+    std::vector<Graph> SubdividedGraphList;
+    for (unsigned j = 0; j < 4; j++) {
+        for (unsigned i = 0; i < 4; i++) {
+
+            Graph OneGraph;
+            for (unsigned gj = 0; gj < 25; gj++) {
+                for (unsigned gi = 0; gi < 25; gi++) {
+                    //i * element + gi;
+                    unsigned x = i * 25 + gi; 
+                    //j * element * tabs * ligne + gj * element * tabs;
+                    unsigned y = j * 25 * 4 * 25 + gj * 25 * 4;
+
+                    //std::cout << (x+y) << std::endl;
+                    OneGraph.addNode(gj*25 + gi, graph.getNodes()[(x+y)]->getMono() );
+                }
+            }
+
+            //Add edges like a suite
+            std::vector<Node*> allNodes = OneGraph.getNodes();
+            for (Node* node : allNodes) {
+                unsigned x = (node->getUId() % 25);
+                unsigned y = (node->getUId() / 25);
+
+                if ((x + 1) + y * 25 < allNodes[allNodes.size() - 1]->getUId()) {
+                    OneGraph.addEdge(x + y * 25, (x + 1) + y * 25);
+                }
+            }
+
+            SubdividedGraphList.push_back(OneGraph);
+        }
+    }
+    return SubdividedGraphList;
+}
+
 int main()
 {
     struct SimpleCapParams capture;
-    capture.mWidth = 150;
-    capture.mHeight = 50;
+    capture.mWidth = 100;
+    capture.mHeight = 100;
     capture.mTargetBuf = new int[ capture.mWidth * capture.mHeight];
 
     if ( !InitCapture(capture) ) { return -1; }
@@ -60,14 +98,55 @@ int main()
 
     doCapture(0);
     
-    InitGraph(graph, capture); // init nodes and edges
+    InitGraph(graph, capture);
+    //InitGraphBarcode(graph, capture); // init nodes and edges
 
     deinitCapture(0);
 
-   // "001100101110100100101";
-    
-    std::string result = ReadBarcode(graph, unsigned(capture.mWidth), unsigned(capture.mHeight));
-    std::cout << result << std::endl;
+    unsigned barcodeCount = 0;
+
+    std::vector<Graph> SubdividedGraphList = SubdivideGraph(graph);
+    for (Graph subdiveGraph : SubdividedGraphList) {
+        
+        subdiveGraph.drawImage(25);
+        
+        bool haveBarcode = HaveBarcode(subdiveGraph, 25, 25);
+        if (haveBarcode) barcodeCount++;
+        
+        std::cout << (haveBarcode ? "BARCODE" : "NOT A BARCODE") << std::endl;
+    }
+
+    //graph.printGraph();
+    graph.drawImage(capture.mWidth);
+    std::cout << "Nombre de codebare trouver dans l'image (peut contenir plusieur fois le meme codebar): " << barcodeCount << std::endl;
 
     return 0;
 }
+
+
+
+/* OLD
+
+//Create a graph special for barcode
+void InitGraphBarcode(Graph& graph, const struct SimpleCapParams& capture) {
+
+    //Add nodes
+    for (unsigned i = 0; i < 3; i++) {
+        for (unsigned j = 0; j < capture.mWidth; j++) {
+            graph.addNode(i * capture.mWidth + j, ConvertRGBBinToMono(capture.mTargetBuf[ (i + int(capture.mWidth*0.33)) * capture.mWidth + j]) );
+        }
+    }
+
+    //Add edges like a suite
+    std::vector<Node*> allNodes = graph.getNodes();
+    for (Node* node : allNodes) {
+        unsigned x = (node->getUId() % capture.mWidth);
+        unsigned y = (node->getUId() / capture.mWidth);
+
+        if ((x + 1) + y * capture.mWidth < allNodes[allNodes.size() - 1]->getUId()) {
+            graph.addEdge(x + y * capture.mWidth, (x + 1) + y * capture.mWidth);
+        }
+    }
+}
+
+*/
